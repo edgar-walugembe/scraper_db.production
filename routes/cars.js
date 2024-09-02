421c7947d55c108cee73069fe20b5e9ee190beb0
PC <edgar.walugembe.upti@gmail.com>
2024-09-02 11:29:29 +0300

    scraper_db 0001

--- routes/cars.js
+++ routes/cars.js
@@ -6,37 +6,37 @@ var router = express.Router();
 /* GENERAL FILTERS */
 //All cars
 router.get("/getAllCars", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("respond with all cars resource");
 });
 
 //Body-type
 router.get("/getCarBodyType", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("respond with a car body type resource");
 });
 
 //Make
 router.get("/getCarMake", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("respond with a car make resource");
 });
 
 //Model
 router.get("/getCarModel", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("respond with a car model resource");
 });
 
 //Year
 router.get("/getCarYear", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("respond with a car year resource");
 });
 
 //Fuel-type
 router.get("/getCarFuelType", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("respond with a car fuel type resource");
 });
 
 //Price-range
 router.get("/getCarPriceRange", function (req, res, next) {
-  res.send("respond with a resource");
+  res.send("respond with a car price range resource");
 });
 
 /* POST CAR REQUESTS*/
