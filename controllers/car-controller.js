9fe7a4ba223ed733497c66c053c5811c15c769e7
PC <edgar.walugembe.upti@gmail.com>
2024-09-24 18:38:50 +0300

    updated selected car route

--- controllers/car-controller.js
+++ controllers/car-controller.js
@@ -86,7 +86,7 @@ async function fetchAllCars(req, res) {
 
 //Get Selected Car
 async function getSelectedCars(req, res) {
-  const carId = req.query.carId;
+  const carId = req.query;
   console.log(`carId is: ${carId}`);
 
   try {
