ba3acc989d241922b7f7ddb6aa0a289cac5b1be3
PC <edgar.walugembe.upti@gmail.com>
2024-09-24 18:31:51 +0300

    updated selected car route

--- routes/cars.js
+++ routes/cars.js
@@ -2,49 +2,42 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const cron = require("node-cron");
+const {
+  createNewCar,
+  fetchAllCars,
+  getSelectedCars,
+} = require("../controllers/car-controller");
 
-//importing functions from the controller
-const { createNewCar, fetchAllCars } = require("../controllers/car-controller");
-
-/* GET CAR REQUESTS */
-
-//All cars
 router.get("/", fetchAllCars);
 
-/* GENERAL FILTERS */
-//Body-type
+router.get("/car", getSelectedCars);
+
 router.get("/body-type", function (req, res, next) {
   res.render("car-body-type", { title: "Cars basing on selected Body type" });
 });
 
-//Make
 router.get("/make", function (req, res, next) {
   res.render("car-make", { title: "Cars basing on selected Makes" });
 });
 
-//Model
 router.get("/model", function (req, res, next) {
   res.render("car-model", { title: "Cars basing on selected Models" });
 });
 
-//Year
 router.get("/year", function (req, res, next) {
   res.render("car-year", { title: "Cars basing on selected Years" });
 });
 
-//Fuel-type
 router.get("/fuel-type", function (req, res, next) {
   res.render("car-fuel-type", { title: "Cars basing on selected Fuel type" });
 });
 
-//Price-range
 router.get("/price-range", function (req, res, next) {
   res.render("car-price-range", {
     title: "Cars basing on selected Price range",
   });
 });
 
-/* POST CAR REQUESTS*/
 router.post("/new-cars", createNewCar);
 
 // Schedule to run daily at 6:00 AM
